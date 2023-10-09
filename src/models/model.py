5d683cb0795bac0505a5a3eda3780e90552bbcd4
dwils152 <dwils152@uncc.edu>
2023-10-09 13:25:28 -0400

    added type hints and docstring to plain model

--- src/models/model.py
+++ src/models/model.py
@@ -1,11 +1,17 @@
-import torch
-#from torch import nn
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn, permute, Tensor, sigmoid
 
 class CrmCNN(nn.Module):
     def __init__(self, config):
+        """
+        Initialize the CrmCNN model.
+
+        Parameters:
+        - config (dict): Configuration dictionary containing model parameters.
+        """
+        
         super(CrmCNN, self).__init__()
+        
+        # Add these as kwargs later ...
         self.batch_size = config['batch_size']
         self.conv_stride = config['conv_stride']
         self.pool_stride = config['pool_stride']
@@ -23,7 +29,7 @@ class CrmCNN(nn.Module):
         self.features = []
         self.data_len = config['training_data_len']
 
-        self.convolutional_layer_1 = nn.Conv2d(
+        self.conv_layer_1 = nn.Conv2d(
             in_channels=1, 
             out_channels=self.kernels,
             dilation=(1, self.dilation),
@@ -32,7 +38,7 @@ class CrmCNN(nn.Module):
             padding='valid'
         )
         
-        self.convolutional_layer_n = nn.Conv2d(
+        self.conv_layer_n = nn.Conv2d(
             in_channels=1, 
             out_channels=self.layer_n_kernels,
             dilation=(1, self.dilation),
@@ -41,16 +47,14 @@ class CrmCNN(nn.Module):
             padding='valid'
         )
         
-        # Different Kernel Initialization Methods
-        #torch.nn.init.ones_(self.convolutional_layer_1.weight)
-        #torch.nn.init.uniform_(self.convolutional_layer_1.weight, a=0.0, b=1.0)
-        
         if self.act_fn == 'relu':
             self.activation = nn.ReLU()
         elif self.act_fn == 'leaky_relu':
             self.activation = nn.LeakyReLU()
-        else:
+        elif self.act_fn == 'gelu':
             self.activation = nn.GELU()
+        else:
+            raise ValueError('Activation function not supported')
             
         self.batch_norm = nn.BatchNorm2d(self.kernels)
         self.batch_norm_n = nn.BatchNorm2d(self.layer_n_kernels)
@@ -68,68 +72,80 @@ class CrmCNN(nn.Module):
         self.dropout = nn.Dropout(p=self.dropout)
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(self.fc_layer_length(), 100)
-        self.fc2 = nn.Linear(100, 1)
+        self.fc2 = nn.Linear(100, 100)
         self.fc3 = nn.Linear(100, 1)
 
-        #save the weights of the first layers's kernels
+        # Save the weights of the first layers's kernels
         self.first_layer_kernels = self.convolutional_layer_1.weight.data.cpu().numpy()
         
-    
-    def feature_hook(self, module, input, output):
+        
+    def feature_hook(self, output: Tensor):
+        """
+        Append the output features to the feature list.
+
+        Parameters:
+        - output (Tensor): The output tensor to be appended to the features.
+        """
+        
         self.features.append(output.detach().cpu().numpy())
 
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Define the forward pass of the CrmCNN model.
+
+        Parameters:
+        - x (Tensor): The input tensor.
+
+        Returns:
+        - Tensor: The output tensor after passing through the network.
+        """
+        
         #print(x.shape)
         x = self.convolutional_layer_1(x)
         #print(x.shape)
         x = self.activation(x)
-        #x = torch.exp(x)
         x = self.batch_norm(x)
-        x = torch.permute(x, (0, 2, 1, 3))
+        x = permute(x, (0, 2, 1, 3))
         #print(x.shape)
         x = self.pooling(x)
         #print(x.shape)
         x = self.dropout(x)
 
-        for i in range(self.num_layers - 1):
+        for layer in range(self.num_layers - 1):
             x = self.convolutional_layer_n(x) # change to convolutional_layer_n
             #print(x.shape)
             x = self.activation(x)
             x = self.batch_norm_n(x)
-            x = torch.permute(x, (0, 2, 1, 3))
+            x = permute(x, (0, 2, 1, 3))
             #print(x.shape)
-            x = self.pooling_n(x) # change to pooling_n
+            x = self.pooling_n(x)
             #print(x.shape)
             x = self.dropout(x)
         
-        # Attention mechanism
-        #attention_logit = torch.matmul(x.squeeze(1), self.attention_weights) + self.attention_bias
-        #attention_weights = F.softmax(attention_logit, dim=1)
-        #x = x * attention_weights.unsqueeze(-1)
-        #x = self.multi_head_attention(x) 
-    
         handle = self.fc1.register_forward_hook(self.feature_hook)
         
         x = self.flatten(x)
         x = self.fc1(x)
         x = self.fc2(x)
+        x = self.fc3(x)
 
         handle.remove() 
-   
-        return torch.sigmoid(x)
-        #return x
+        return sigmoid(x)
 
-    def fc_layer_length(self):
-        # Initialize the output length to the input size
+
+    def fc_layer_length(self) -> int:
+        """
+        Calculate the length of the flattened tensor before the fully connected layer.
+
+        Returns:
+        - int: The total number of elements in the output tensor before the fully connected layer.
+        """
+        
         output_length = self.data_len
 
-        # Apply the convolutional and pooling layers
         for i in range(self.num_layers):
-            # Apply the convolutional layer
             output_length = (output_length - self.dilation * (self.kernel_len - 1) - 1) // self.conv_stride + 1
-            # Apply the max pooling layer
             output_length = (output_length - self.pool_size) // self.pool_stride + 1
 
-        # Return the total number of elements in the output tensor
         return output_length #* self.kernels
