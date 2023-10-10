b1414e106b4371e9b594ffafed6ca84fa0c808f6
dwils152 <dwils152@uncc.edu>
2023-10-10 10:51:41 -0400

    update

--- src/models/LightningCrmCNN.py
+++ src/models/LightningCrmCNN.py
@@ -1,42 +1,32 @@
 import pytorch_lightning as pl
-from torch import optim, nn, squeeze, unsqueeze
+from torch import nn, unsqueeze, squeeze
+import torch.optim as optim
 
 class LightningCrmCNN(pl.LightningModule):
     def __init__(self, crm_cnn):
         super().__init__()
         self.crm_cnn = crm_cnn
+        self.criterion = nn.BCELoss()
+
+    def _shared_step(self, batch, stage):
+        inputs, labels = batch
+        inputs = unsqueeze(inputs, 1).float()
+        labels = squeeze(labels).float()
+        predictions = self.crm_cnn(inputs)
+        predictions = squeeze(predictions)
+        loss = self.criterion(predictions, labels)
+        self.log(f'{stage}_loss', loss, sync_dist=True, on_step=True, on_epoch=True)
+        return loss
 
     def training_step(self, batch, batch_idx):
-        inputs, labels = batch
-        # Make the inputs and labels the correct dimensions
-        inputs = unsqueeze(inputs, 1).float()
-        labels = squeeze(labels).float()
-        predictions = self.crm_cnn(inputs)
-        predictions = squeeze(predictions) 
-        loss = nn.BCELoss()(predictions, labels)
-        self.log('train_loss', loss, sync_dist=True, on_step=True, on_epoch=True)
-        return loss
+        return self._shared_step(batch, 'train')
     
     def validation_step(self, batch, batch_idx):
-        inputs, labels = batch
-        # Make the inputs and labels the correct dimensions
-        inputs = unsqueeze(inputs, 1).float()
-        labels = squeeze(labels).float()
-        predictions = self.crm_cnn(inputs)
-        predictions = squeeze(predictions) 
-        val_loss = nn.BCELoss()(predictions, labels)
-        self.log('val_loss', val_loss, sync_dist=True, on_step=True, on_epoch=True)
+        self._shared_step(batch, 'val')
     
     def test_step(self, batch, batch_idx):
-        inputs, labels = batch
-        # Make the inputs and labels the correct dimensions
-        inputs = unsqueeze(inputs, 1).float()
-        labels = squeeze(labels).float()
-        predictions = self.crm_cnn(inputs)
-        predictions = squeeze(predictions) 
-        test_loss = nn.BCELoss()(predictions, labels)
-        self.log('test_loss', test_loss, sync_dist=True, on_step=True, on_epoch=True)
+        self._shared_step(batch, 'test')
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = optim.Adam(self.trainer.model.parameters(), lr=1e-3)
         return optimizer
--- src/models/train.py
+++ src/models/train.py
@@ -9,8 +9,14 @@ import pytorch_lightning as pl
 import optuna
 from optuna.integration import PyTorchLightningPruningCallback
 
+import random
+import numpy as np
+import torch
+
 def objective(trial):
     
+    #set_seed(42)
+
     data = MMapDataset(seqs_path=args.seqs,
                     labels_path=args.labels, 
                     fasta_path=args.fasta,
@@ -23,38 +29,51 @@ def objective(trial):
     test_loader = get_data_loader(splits[2], batch_size=32)
     
     config = {
-        "batch_size": 16,
-        "conv_stride": 1,
-        "pool_stride": 1,
-        "pool_size": 3,
-        "dropout": 0.25,
-        "kernels": 100,
-        "layer_n_kernels": 5,
-        "kernel_len": 8,
-        "learning_rate": 1e-4,
-        "weight_decay": 1e-6,
-        "momentum": 0.95,
-        "dilation": 1,
-        "num_layers": 2,
-        "act_fn": 'leaky_relu',
+        "batch_size": trial.suggest_categorical("batch_size", [16, 32, 64, 128]),
+        "conv_stride": trial.suggest_int("conv_stride", 1, 1),
+        "pool_stride": trial.suggest_int("pool_stride", 1, 1),
+        "pool_size": trial.suggest_int("pool_size", 1, 3),
+        "dropout": trial.suggest_float("dropout", 0.0, 0.7, step=0.05),
+        "kernels": trial.suggest_int("kernels", 50, 100),
+        "layer_n_kernels": trial.suggest_int("layer_n_kernels", 50, 100),
+        "kernel_len": trial.suggest_int("kernel_len", 5, 15),
+        "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-1, log=True),
+        "weight_decay": trial.suggest_float("weight_decay", 1e-5, 1e-1, log=True),
+        "momentum": trial.suggest_float("momentum", 0.0, 1.0, step=0.05),
+        "dilation": trial.suggest_int("dilation", 1, 1),
+        "num_layers": trial.suggest_int("num_layers", 1, 3),
+        "act_fn": trial.suggest_categorical("act_fn", ["relu"]),
         "training_data_len": 200
     }
     
     model = LightningCrmCNN(CrmCNN(config))
+    
+    # Create a DDP strategy
+    #strategy = pl.strategies.SingleDeviceStrategy(device=torch.device("cuda:0"))
+
     trainer = pl.Trainer(max_epochs=args.epochs,
                         accelerator='gpu',
-                        devices=args.gpus,
+                        devices=1,
                         num_nodes=1,
-                        strategy='ddp',
+                        strategy='auto',
                         logger=True)
     
     trainer.fit(model, train_loader, val_loader)
+    return trainer.callback_metrics["val_loss"].item()
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+set_seed(42)  # Use a fixed seed
 
 def main(args):
      
-    pruner = optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner()
-    study = optuna.create_study(direction="maximize", pruner=pruner)
-    study.optimize(objective, n_trials=100, timeout=600)
+    #pruner = optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner()
+    study = optuna.create_study(direction="minimize") #pruner=pruner)
+    study.optimize(objective, n_trials=100)
     
     print("Number of finished trials: {}".format(len(study.trials)))
     print("Best trial:")
